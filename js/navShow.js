339c259f1ccd9e994d4e70122f7ea038445961f5
Zack Woodmansee <woodmandev@gmail.com>
2014-05-21 17:37:18 -0600

    fixed scroll issues on nav, nav popout

--- js/navShow.js
+++ js/navShow.js
@@ -1,6 +1,16 @@
 jQuery(document).ready(function($){
-    $('#navNav').click(function(){
-		$('.navBody').toggleClass('hide');
-		$(this).toggleClass('navOpened');
+    $('#navNav, .navBar').click(function(){
+        var $lefty = $('.navBody');
+        $lefty.animate({
+            right: parseInt($lefty.css('right'),10) == 0 ?
+                -$lefty.outerWidth() :
+                0
+        });
+        var $nav = $('#navNav');
+        $nav.animate({
+            right: parseInt($nav.css('right'),10) == 50 ?
+                +230 :
+                50
+        });
     });
 });
